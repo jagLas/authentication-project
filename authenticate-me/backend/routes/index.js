63792500e47f78610477624aa20ddbfb1acb2d43
jagLas <jglasher@gmail.com>
2023-04-19 13:57:51 -0400

    phase 1 api router setup

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api')
 
 // router.get('/hello/world', (req, res) => {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -12,6 +13,8 @@ router.get("/api/csrf/restore", (req, res) => {
     res.status(200).json({
       'XSRF-Token': csrfToken
     });
-  });
+});
+
+router.use('/api', apiRouter)
 
 module.exports = router;
