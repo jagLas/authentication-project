a2c0de0b621a5bfb594384b5e8b45efc5d13ada5
jagLas <jglasher@gmail.com>
2023-04-20 10:18:07 -0400

    phase 3 complete middleware auth functions

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const {setTokenCookie, restoreUser, requireAuth} = require('../../utils/auth.js');
+const {User} = require('../../db/models');
+// const {restoreUser} = require('../../utils/auth.js')
+
+/*
+//routest for testing middleware auth functions
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'demo-user'
+    }
+  })
+
+  setTokenCookie(res, user);
+  return res.json({user})
+})
+
+router.get('/restore-user', restoreUser, (req, res) => {
+  return res.json(req.user)
+})
+
+router.get('/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+)
+*/
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
-  });
+});
 
 module.exports = router;
