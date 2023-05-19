80ab8a94ff6534f2212d80127c5c1ee2e03e683e
jagLas <jglasher@gmail.com>
2023-05-19 11:18:35 -0400

    basic dropdown menu started

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -1,13 +1,42 @@
-function ProfileButton () {  
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { signout } from "../../store/session";
+
+function ProfileButton ({user}) { 
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+
+    const logout = async () => {
+        try {
+            await dispatch(signout());
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    //todo: close when outside click is made
 
     return (
-        <i
-            className="fas fa-user"
-            style={{
-                color: '#00216f',
-                fontSize: '48px'
-            }}
-        ></i>
+        <>
+            <button onClick={() => setShowMenu(!showMenu)}>
+            <i
+                className="fas fa-user"
+                style={{
+                    color: '#00216f',
+                    fontSize: '48px'
+                }}
+            ></i>
+            </button>
+            {showMenu ?
+                <>
+                    <ul>
+                        <li>{user.username}</li>
+                        <li>{user.email}</li>
+                        <li><button onClick={logout}>Logout</button></li>
+                    </ul>
+                </>
+             : null}
+        </>
     )
 }
 
--- authenticate-me/frontend/src/components/Navigation/index.js
+++ authenticate-me/frontend/src/components/Navigation/index.js
@@ -1,23 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
-import { signout } from "../../store/session";
 import ProfileButton from "./ProfileButton";
 
-function Navigation() {
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user);
 
-    const logout = async () => {
-        try {
-            await dispatch(signout());
-        } catch (e) {
-            console.log(e)
-        }
-    }
+function Navigation() {
+    const user = useSelector(state => state.session.user);
 
     return (
         <nav>
-
             <NavLink to='/'>Home</NavLink>
             {user ? null :
                 <>
@@ -25,8 +15,7 @@ function Navigation() {
                     <NavLink to='/signup'>Signup</NavLink>
                 </>
             }
-            {user ? <ProfileButton/> : null}
-            {user ? <button onClick={logout}>Logout</button> : null}
+            {user ? <ProfileButton user={user}/> : null}
         </nav>
     )
 }
